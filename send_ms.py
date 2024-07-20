324617787c6520cf8cc26fb25d3620ce872a3f88
Eugene-S-Hwang <egswhwang@gmail.com>
2024-07-20 16:33:37 -0400

    Testing

---
+++ send_ms.py
@@ -0,0 +1,9 @@
+
+
+# message = client.messages.create(
+#     body="Testing Testing",
+#     from_=os.environ["TWILIO_CH_NUM"],
+#     to=os.environ["CH_NUM"],
+# )
+
+# print(message.body)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import os
+from twilio.rest import Client
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+account_sid = os.environ["TWILIO_SID"]
+auth_token = os.environ["TWILIO_TOKEN"]
+client = Client(account_sid, auth_token)
+
+st.title("For Chaemin")
+if(st.button("Press for a surprise!", type="primary")):
+    # message = client.messages.create(
+    #     body="Testing Testing",
+    #     from_=os.environ["TWILIO_CH_NUM"],
+    #     to=os.environ["CH_NUM"],
+    # )
+    st.write("works")
