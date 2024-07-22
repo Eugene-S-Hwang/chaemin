6613a4224ee5d3da102e1663e2c0417f18fefd9a
Eugene-S-Hwang <egswhwang@gmail.com>
2024-07-22 01:01:48 -0400

    bye twilio

--- send_ms.py
+++ send_ms.py
@@ -1,9 +1,16 @@
+# import os
+# from twilio.rest import Client
 
+# # def send_msg():
+# account_sid = os.environ["TWILIO_SID"]
+# auth_token = os.environ["TWILIO_TOKEN"]
+# client = Client(account_sid, auth_token)
 
 # message = client.messages.create(
 #     body="Testing Testing",
 #     from_=os.environ["TWILIO_CH_NUM"],
-#     to=os.environ["CH_NUM"],
+#     # to=os.environ["CH_NUM"],
+#     to="+12019129586"
 # )
 
 # print(message.body)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,7 @@
 import streamlit as st
-# import os
-# from twilio.rest import Client
-
-# account_sid = os.environ["TWILIO_SID"]
-# auth_token = os.environ["TWILIO_TOKEN"]
-# client = Client(account_sid, auth_token)
+# import send_ms
 
 st.title("For Chaemin")
 if(st.button("Press for a surprise!", type="primary")):
-    # message = client.messages.create(
-    #     body="Testing Testing",
-    #     from_=os.environ["TWILIO_CH_NUM"],
-    #     to=os.environ["CH_NUM"],
-    # )
+    # send_ms.send_msg()
     st.write("works")
