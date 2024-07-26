d61e6f2bb159e7bfe83052fd1dab1dd2ba7c9dce
Eugene-S-Hwang <egswhwang@gmail.com>
2024-07-25 22:39:32 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,12 @@ import streamlit as st
 import send_email
 
 
-receiver = st.secrets["CH_EMAIL"]
+# receiver = st.secrets["CH_EMAIL"]
 
-sender = st.secrets["CH2EMAIL"]
-st.title("For Chaemin")
-if(st.button("Press for a surprise!", type="primary")):
-    send_email.send_email(receiver, sender)
-    st.write("sent!")
+# sender = st.secrets["CH2EMAIL"]
+# st.title("For Chaemin")
+# if(st.button("Press for a surprise!", type="primary")):
+#     send_email.send_email(receiver, sender)
+#     st.write("sent!")
+
+st.write(st.secrets)
