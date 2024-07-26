d3c1b5ee861b559461722bba373ee2f5b67ef3ac
Eugene-S-Hwang <egswhwang@gmail.com>
2024-07-25 22:30:23 -0400

    environ

--- send_email.py
+++ send_email.py
@@ -3,15 +3,14 @@ import streamlit as st
 import random
 import os
 
-receiver = st.secrets["CH_EMAIL"]
 
-yag = yagmail.SMTP(st.secrets["CH2EMAIL"], oauth2_file='~/Work/oauth2.json')
 
-def send_email():
+def send_email(receiver, sender):
 
     body = ["I love you so much!", "You are doing great. Keep going, I am right next to you.", "You are so so pretty :D", "You're the best and you make me feel so happy. I feel so loved when I'm with you",
             "If I'm not with you, I miss you.", "I can trust you with my life, my love. I can trust you with everything."]
 
+    yag = yagmail.SMTP(sender, oauth2_file='~/Work/oauth2.json')
     yag.send(
         to=receiver,
         subject="test",
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import send_email
 
+
+receiver = st.secrets["CH_EMAIL"]
+
+sender = st.secrets["CH2EMAIL"]
 st.title("For Chaemin")
 if(st.button("Press for a surprise!", type="primary")):
-    send_email.send_email()
+    send_email.send_email(receiver, sender)
     st.write("sent!")
